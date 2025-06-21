87535b83182eb6881bbe8ccaebe5104a21ec2b41
ismailrowaihy <ismailrowaihy@gmail.com>
2025-06-21 15:09:07 +0000

    added c function that measures the balance factor of a btree

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+*binary_tree_height_new- helper for balance factor
+*@tree: tree to check
+*Return: the balance of a tree
+*/
+int binary_tree_height_new(const binary_tree_t *tree)
+{
+int h_right;
+int h_left;
+h_right = 0;
+h_left = 0;
+
+if (tree)
+{
+
+if (tree->left)
+h_left = 1 + binary_tree_height(tree->left);
+
+if (tree->right)
+h_right = 1 + binary_tree_height(tree->right);
+
+return (h_left - h_right);
+}
+return (0);
+}
+
+/**
+*binary_tree_balance- gives the balance of a tree
+*@tree: tree to check
+*Return: the balance factor
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+if (tree)
+{
+return (binary_tree_height_new(tree));
+}
+else
+return (0);
+}
