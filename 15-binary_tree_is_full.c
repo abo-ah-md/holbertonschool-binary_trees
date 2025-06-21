a1b486f619c0885b30c357913b7c56ae9966097f
abo-ah-md <abo-ah-md1@hotmail.com>
2025-06-21 20:06:31 +0300

    added a c function to check binary tree fullness

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+*binary_tree_is_full - gives the balance of a
+* treechecks the tree is full or not
+*@tree: tree to check
+*Return: 1 if tree is full 0 if not
+*/
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+int check;
+check = 1;
+
+if (tree)
+{
+
+if (tree->left)
+check = binary_tree_is_full(tree->left);
+
+if (tree->right)
+check = binary_tree_is_full(tree->right);
+
+if (check == 0)
+return (0);
+
+if (!tree->left && !tree->right)
+return (2);
+
+if (tree->left && !tree->right)
+return (0);
+
+if (tree->right && !tree->left)
+return (0);
+
+if (tree->left && tree->right)
+return (1);
+}
+return (0);
+}
