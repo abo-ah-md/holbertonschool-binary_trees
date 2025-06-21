e824133ea8a58615417b8d95e0857034bf13455a
ismailrowaihy <ismailrowaihy@gmail.com>
2025-06-21 14:31:10 +0000

    added C function that counts the nodes with at least 1 kid

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+*binary_tree_nodes- counts the leaves of a tree
+*@tree: tree to check
+*Return: the number of the tree leaves
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+
+int nodes;
+nodes = 0;
+if (tree)
+{
+nodes +=  binary_tree_nodes(tree->right);
+nodes +=  binary_tree_nodes(tree->left);
+if (!tree->left && !tree->right)
+return (0);
+
+return (nodes + 1);
+}
+else
+return(0);
+}
